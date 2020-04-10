7eef3cc24bac6f1e4ee04550a1323b838da57fbe
ashumsd7 <40313523+ashumsd7@users.noreply.github.com>
2020-04-10 18:28:33 +0530

    Simple jitsi poc
    embeded in browser

---
+++ app.js
@@ -0,0 +1,20 @@
+new Vue({
+    el: "#app",
+    data: {
+      DoctorName: " Dr Brick Wall.",
+      pName: "Steve Cramer",
+      pAge: 55,
+      pBP: "130/84",
+      pHistory : "Chronic Fatigue"
+    },
+    methods: {
+       
+       
+
+
+
+    }
+});
+
+
+
